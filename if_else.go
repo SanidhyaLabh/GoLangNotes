640cf0550a57fa2ad6373774c2957c9478000afe
OneKreation <95623860+SanidhyaLabh@users.noreply.github.com>
2025-12-11 19:18:07 +0530

    Add files via upload

---
+++ if_else.go
@@ -0,0 +1,46 @@
+package main
+
+func main() {
+	// age := 16
+
+	// if age >= 18 {
+	// 	fmt.Println("person is an adult")
+	// } else {
+	// 	fmt.Println("person is a minor")
+	// }
+
+	// age := 11
+	// if age >= 18 {
+	// 	fmt.Println("person is an adult")
+	// } else if age >= 12 {
+	// 	fmt.Println("person is teenager")
+	// } else {
+	// 	fmt.Println("person is a child")
+	// }
+
+	// var role = "admin"
+	// var hasPermissions = true
+	// if role == "admin" || hasPermissions {
+	// 	fmt.Println("User has admin privileges.")
+	// }
+
+	// we can declare a variable inside if construct
+	// if age := 15; age >= 18 {
+	// 	fmt.Println("person is an adult", age)
+	// } else if age >= 12 {
+	// 	fmt.Println("person is teenager", age)
+	// } else {
+	// 	fmt.Println("person is a child", age)
+	// }
+
+	//go doesn't have ternary operator, you will have to use normal if else
+
+	// age := 20
+	// var status string
+	// if age >= 18 {
+	// 	status = "adult"
+	// } else {
+	// 	status = "minor"
+	// }
+	// fmt.Println("Status:", status)
+}
