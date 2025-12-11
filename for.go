bb81b3da03437f5ee42aa1cfd2e4dd1003bab358
OneKreation <95623860+SanidhyaLabh@users.noreply.github.com>
2025-12-11 19:17:52 +0530

    Add files via upload

---
+++ for.go
@@ -0,0 +1,36 @@
+package main
+
+//for -> only construct in go for looping
+
+func main() {
+	//while loop
+	//i := 1
+	//for i <= 3 {
+	//	fmt.Println(i)
+	//	i = i + 1
+	//}
+
+	// infinite loop
+	// for{
+	// 	println("1")
+	// }
+
+	//classic for loop
+	// for i := 0; i <= 3; i++ {
+	// 	fmt.Println(i)
+	// }
+
+	// for i := 0; i <= 3; i++ {
+	// 	//break
+	// 	if i == 2 {
+	// 		continue    //to skip the current iteration
+	// 	}
+	// 	fmt.Println(i)
+	// }
+
+	// 1.22 range -> to iterate over arrays, slices, maps, strings
+	// for i := range 11 {
+	// 	fmt.Println(i)
+	// }
+
+}
