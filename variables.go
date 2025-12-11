5f8bbab113cb059bf92f79b83ad71c28e0c0d4fb
OneKreation <95623860+SanidhyaLabh@users.noreply.github.com>
2025-12-11 19:17:14 +0530

    Add files via upload

---
+++ variables.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var name string = "Sanidhya"
+
+	// infer
+	// var name = "Sanidhya"
+	//var isAdmin = false
+
+	//x := 10.5
+	//name := "Sanidhya"
+
+	//var name string
+	//name = "Sanodhya"
+
+	//var price float32 = 50.5
+	//var price = 50.5
+	//price := 50.5
+
+	fmt.Println(price)
+
+}
