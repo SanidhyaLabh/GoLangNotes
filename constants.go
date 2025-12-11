2716fbeb1d270f372859da2ef822a76349dc402c
OneKreation <95623860+SanidhyaLabh@users.noreply.github.com>
2025-12-11 19:17:35 +0530

    Add files via upload

---
+++ constants.go
@@ -0,0 +1,22 @@
+package main
+
+// const age = 30
+
+//It can be declared outside functions
+//name := "golang" // This line would cause a compilation error since short variable declarations are not allowed outside functions
+//var name = "golang"
+
+func main() {
+	//const name = "Sanidhya"
+	// name = "JavaScript" // This line would cause a compilation error since constants cannot be reassigned
+
+	//const age = 30
+	//fmt.Println(age)
+
+	// to group :  const (
+	//port = 5000
+	//host = "localhost"
+	//)
+	//fmt.Println(port, host)
+
+}
